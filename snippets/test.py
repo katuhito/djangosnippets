c1ce712b3ec947b373b0db45c43c93b01e82dbb6
Katuhito Hara <katuhitohara@gmail.com>
2021-07-28 10:49:03 +0900

    HTML

--- snippets/test.py
+++ snippets/test.py
@@ -24,18 +24,32 @@ from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 #         response = self.client.get("/")
 #         self.assertEqual(response.content, b"Hello World")
 
-class CreateSnippetTest(TestCase):
-    def test_should_resolve_snippet_new(self):
-        found = resolve("/snippets/new/")
-        self.assertEqual(snippet_new, found.func)
+# class CreateSnippetTest(TestCase):
+#     def test_should_resolve_snippet_new(self):
+#         found = resolve("/snippets/new/")
+#         self.assertEqual(snippet_new, found.func)
 
-class SnippetDetailTest(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1/")
-        self.assertEqual(snippet_detail, found.func)
+# class SnippetDetailTest(TestCase):
+#     def test_should_resolve_snippet_detail(self):
+#         found = resolve("/snippets/1/")
+#         self.assertEqual(snippet_detail, found.func)
 
-class EditSnippetTest(TestCase):
-    def test_should_resolve_snippet_edit(self):
-        found = resolve("/snippets/1/edit/")
-        self.assertEqual(snippet_edit, found.func)
+# class EditSnippetTest(TestCase):
+#     def test_should_resolve_snippet_edit(self):
+#         found = resolve("/snippets/1/edit/")
+#         self.assertEqual(snippet_edit, found.func)
+
+
+#トップページがb"Hello World"というプレーンテキストではなくHTMLファイルを返す等に変更するため、
+#TopPageViewTestクラスは削除して、TopPageTestクラスを次のように書き換える。
+
+class TopPageTest(TestCase):
+    def test_top_page_returns_200_and_expected_title(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Django スニペット", status_code=200)
+
+    def test_top_page_uses_expected_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "snippets/top.html")
+
 
