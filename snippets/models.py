bbae10e1e9e36ac6e2c158f9cdeac7c41cac9f63
Katuhito Hara <katuhitohara@gmail.com>
2021-07-28 10:19:00 +0900

    models django admin

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Snippet(models.Model):
+    title = models.CharField('タイトル', max_length=128)
+    code = models.TextField('コード', blank=True)
+    description = models.TextField('説明', blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='投稿者', on_delete=models.CASCADE)
+    created_at = models.DateTimeField('投稿日', default=timezone.now)
+    updated_at = models.DateTimeField('更新日', default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
