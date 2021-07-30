d3ed8aadeb3261b04fb88adc58e374baf8eb1fca
Katuhito Hara <katuhitohara@gmail.com>
2021-07-30 11:16:38 +0900

    Django-Bootstrap5

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
+    path('', views.top, name="top"),
     path('snippets/new/', views.snippet_new, name="snippet_new"),
     path('snippets/<int:snippet_id>/', views.snippet_detail, name="snippet_detail"),
     path('snippets/<int:snippet_id>/edit/', views.snippet_edit, name="snippet_edit"),
